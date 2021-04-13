fcbf008c2b7230809ba992d9ec6215e586fdbdc7
Konstantin Intsyn <intsyn@mail.ru>
2021-04-13 23:02:23 +0300

    добавил параметры в базу, модель параметров в коде и пример работы с ним

--- ConsoleApp/DBConnector.cs
+++ ConsoleApp/DBConnector.cs
@@ -6,48 +6,65 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using System.Data;
+using System.Data.Linq;
 
 namespace ConsoleApp
 {
     class DBConnector
     {
+
+        private static string conn_str = ConsoleApp.Properties.Settings.Default.ConnectionString;
+
+
+        public static void GetParam(string Name)
+        {
+            using (OleDbConnection connection = new OleDbConnection(conn_str))
+            {
+                DataContext db = new DataContext(connection);
+                Table<Param> params_ = db.GetTable<Param>();
+                Param a = params_.ToList().First(par => par.Name == "B");
+            }
+        }
+
         public static void Test()
         {
+            OleDbConnection connection = new OleDbConnection(conn_str);
+            DataContext db = new DataContext(connection);
+            Table<Param> params_ = db.GetTable<Param>();
+            Param a = params_.ToList().First(par => par.Name == "B");
 
-            // Create and open the connection in a using block. This
-            // ensures that all resources will be closed and disposed
-            // when the code exits.
+            Console.WriteLine($"{a.Name} {a.Min} {a.Max} {a.Step}");
+
+            //foreach (var user in params_)
+            //{
+            //    Console.WriteLine("{0} \t{1} \t{2}", user.Min, user.Max, user.Step);
+            //}
+        }
+
+        public static DataTable GetImageAreas()
+        {
+            var ds = RunSelect("Select * From `Область изображение`");
+            return ds.Tables[0];
+        }
+
+        public static DataSet GetParams()
+        {
+            return RunSelect("Select * From `Параметры`");
+            //return ds.Tables[0];
+        }
+
+        public static DataSet RunSelect(string cmd)
+        {
             using (OleDbConnection connection = new OleDbConnection(ConsoleApp.Properties.Settings.Default.ConnectionString))
             {
-                OleDbCommand command = new OleDbCommand("Select * From `Область изображение`");
-
-                // Set the Connection to the new OleDbConnection.
+                OleDbCommand command = new OleDbCommand(cmd);
                 command.Connection = connection;
-
-                // Open the connection and execute the insert command.
-                try
-                {
-                    connection.Open();
-                    var reader = command.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        var a = reader[2];
-                        Console.WriteLine($"{reader[1]} {reader[2]} {reader[3]} {reader[4]} {reader[5]}");
-                    }
-                    reader.Close();
-
-                    //foreach (var item in response)
-                    //    Console.WriteLine(((System.Data.Common.DataRecordInternal)item)[1]);
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                // The connection is automatically closed when the
-                // code exits the using block.
-
+                connection.Open();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd, connection);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                return ds;
             }
         }
     }
---
+++ ConsoleApp/Param.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Linq.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp
+{
+    [Table(Name = "Параметры")]
+    public class Param
+    {
+        [Column(Name = "Код параметра", IsPrimaryKey = true, IsDbGenerated = true)]
+        public string Code { get; set; }
+
+        [Column(Name = "Наименование")]
+        public string Name { get; set; }
+
+        [Column(Name = "Нижняя граница")]
+        public int Min { get; set; }
+
+        [Column(Name = "Верхняя граница")]
+        public int Max { get; set; }
+
+        [Column(Name = "Шаг")]
+        public int Step { get; set; }
+
+        private IEnumerable<int> AsEnumerable()
+        {
+            int x = Min;
+            do
+            {
+                yield return x;
+                x += Step;
+                if (Step < 0 && x <= Max || 0 < Step && Max <= x)
+                    break;
+            }
+            while (true);
+        }
+    }
+}
